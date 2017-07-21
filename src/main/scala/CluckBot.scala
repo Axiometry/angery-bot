7b6bf81479bfb877932bf9c49a2f00c7b691e7cd
Axiometry <marco.vanhilst@gmail.com>
2017-07-20 17:33:49 -0700

    Clean up CluckBot

--- src/main/scala/CluckBot.scala
+++ src/main/scala/CluckBot.scala
@@ -2,15 +2,23 @@ import io.scalac.slack._
 import io.scalac.slack.bots._
 import io.scalac.slack.common._
 
+import scala.util.Random
+
 class CluckBot(override val bus: MessageEventBus) extends IncomingMessageListener {
   def receive: Receive = {
     case bm @ BaseMessage(text, channel, user, dateTime, edited) =>
       BotInfoKeeper.current match {
-        case Some(botInfo) => Seq(s"<@${botInfo.id}>: ", s"<@${botInfo.id}> ").find(text startsWith _).map { trigger =>
-          val result = text.drop(trigger.length).trim
-          if(result.nonEmpty)
-            publish(OutboundMessage(channel, result.map(ch => if(scala.util.Random.nextBoolean()) ch.toUpper else ch.toLower)))
-        }
+        case Some(botInfo) =>
+          Seq(s"<@${botInfo.id}>: ", s"<@${botInfo.id}> ")
+            .find(text startsWith _)
+            .map { trigger =>
+              val targetText = text.drop(trigger.length).trim
+
+              if(targetText.nonEmpty) {
+                val cluckedText = targetText.map(ch => if(Random.nextBoolean) ch.toUpper else ch.toLower)
+                publish(OutboundMessage(channel, cluckedText))
+              }
+            }
         case None =>
       }
   }
